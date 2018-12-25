767dca9b2a5984ea571d2dc3e62a99c028159748
sixfacearu <sixface0324@gmail.com>
2018-12-25 22:32:49 +0530

    push

--- index.js
+++
@@ -1,7 +0,0 @@
-const express=require('express');
-const app=express();
-app.get('/', function (req, res) {
-    res.sendFile('index.html', { root: __dirname });
-});
-var port = process.env.PORT || 3000;
-app.listen(port);
